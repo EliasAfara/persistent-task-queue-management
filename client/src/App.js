9de5dc4b59f25df5a0b64af6c94e509a2719e4c1
Elias Afara <afaraelias@gmail.com>
2022-06-12 19:24:20 +0200

    Frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <h1>Red Alert Labs Technical Assessment</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
