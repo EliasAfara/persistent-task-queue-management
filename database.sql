20223d5f2330cfcd641b6aa0c70fe3b34696fffc
Elias Afara <afaraelias@gmail.com>
2022-06-12 02:00:15 +0200

    Queries modification

--- database.sql
+++ database.sql
@@ -5,11 +5,16 @@ CREATE TABLE callback(
     label VARCHAR(255) UNIQUE NOT NULL
 )
 
+CREATE TYPE eventState AS ENUM ('processing', 'failed', 'finished');
+
 CREATE TABLE events(
     event_id SERIAL PRIMARY KEY,
     type VARCHAR(255) NOT NULL,
+    state eventState NOT NULL DEFAULT 'processing',
     data json,
-    createdAt timestamp with time zone NOT NULL DEFAULT now(),
+    scheduled_for_time VARCHAR(255) DEFAULT '1 second',
+    scheduled_for TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
     callback_id INTEGER NOT NULL REFERENCES callback(callback_id)
 )
 
@@ -26,3 +31,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_event_trigger AFTER INSERT on events
 FOR EACH ROW EXECUTE PROCEDURE notify_event();
+
+DROP TRIGGER update_event_trigger ON events;
+
+DROP FUNCTION notify_event();
